90624728d5574b753a039ccbdcd2cbd2869ad7d0
shepard153 <kamil.kosmider1@gmail.com>
2022-07-02 18:25:34 +0200

    Procfile for Heroku

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
